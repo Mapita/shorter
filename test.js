09c7c9352ccd7d8a1a8c7853cce9f4eb9a96cf44
Sophie Kirschner <sophie@thehumangeo.com>
2018-04-17 10:37:08 +0300

    Add more tests for much improved coverage

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const assert = require("assert");
 const axios = require("axios");
 const canary = require("canary-test");
+const moment = require("moment");
 
 const shorter = new canary.Group("Shorter");
 
@@ -16,6 +17,7 @@ module.exports = app => {
         const response = await axios({
             method: method,
             url: `${app.config.unitTestHostUrl}${url}`,
+            validateStatus: status => !!status,
             data: data,
         })
         return response.data;
@@ -36,15 +38,14 @@ module.exports = app => {
             assert(response.response.url === "http://www.google.com");
             assert(linkEnding);
             assert(response.response.shortUrl === `${app.config.hostName}/${linkEnding}`);
-            assert(response.response.tags instanceof Array);
-            assert(response.response.tags.length === 0);
+            assert(response.response.tags && response.response.tags.length === 0);
             assert(response.response.creationTime);
             assert(response.response.lastModifiedTime);
         });
         this.test("visit", async function(){
             const response = await request("get", `/${linkEnding}`);
             // Assumes that the google.com homepage is accessible and normal
-            assert(response.startsWith("<!doctype html>"));
+            assert(response.indexOf("google"));
         });
         this.test("wait for click to be inserted into the database", function(){
             // Important because the server doesn't wait for the insertion to
@@ -57,9 +58,145 @@ module.exports = app => {
             });
             assert(response.success);
             assert(response.response.ending === linkEnding);
-            assert(response.response.clicks instanceof Array);
-            assert(response.response.clicks.length === 1);
+            assert(response.response.clicks && response.response.clicks.length === 1);
         });
+    });
+    
+    shorter.series("shorten one with custom ending", function(){
+        this.test("shorten", async function(){
+            const response = await request("post", "/api/v1/shorten", {
+                url: "http://www.google.com",
+                ending: "testending",
+            });
+            assert(response.success);
+            assert(response.response.url === "http://www.google.com");
+            assert(response.response.ending === "testending");
+        });
+        this.test("visit", async function(){
+            const response = await request("get", `/testending`);
+            // Assumes that the google.com homepage is accessible and normal
+            assert(response.indexOf("google"));
+        });
+    });
+    
+    shorter.series("shorten one then update", function(){
+        let linkEnding;
+        let lastTime;
+        this.test("shorten", async function(){
+            const response = await request("post", "/api/v1/shorten", {
+                url: "http://www.google.com",
+            });
+            linkEnding = response.response.ending;
+            lastTime = moment(response.response.creationTime);
+            assert(response.success);
+            assert(response.response.url === "http://www.google.com");
+            assert(response.response.tags && response.response.tags.length === 0);
+            assert(linkEnding);
+            assert(lastTime);
+        });
+        this.test("update tags", async function(){
+            const response = await request("post", "/api/v1/update", {
+                ending: linkEnding,
+                tags: ["firstTag", "secondTag"],
+            });
+            assert(response.success);
+            assert(response.response.url === "http://www.google.com");
+            assert(response.response.tags && response.response.tags.length === 2);
+            assert(response.response.tags[0] === "firstTag");
+            assert(response.response.tags[1] === "secondTag");
+            const newTime = moment(response.response.lastModifiedTime);
+            assert(newTime > lastTime);
+            lastTime = newTime;
+        });
+        this.test("update url", async function(){
+            const response = await request("post", "/api/v1/update", {
+                ending: linkEnding,
+                url: "http://www.maptionnaire.com",
+            });
+            assert(response.success);
+            assert(response.response.url === "http://www.maptionnaire.com");
+            assert(response.response.tags && response.response.tags.length === 2);
+            const newTime = moment(response.response.lastModifiedTime);
+            assert(newTime > lastTime);
+            lastTime = newTime;
+        });
+        this.test("get link properties", async function(){
+            const response = await request("post", "/api/v1/get", {
+                ending: linkEnding,
+            });
+            assert(response.success);
+            assert(response.response.url === "http://www.maptionnaire.com");
+            assert(response.response.tags && response.response.tags.length === 2);
+            assert(response.response.tags[0] === "firstTag");
+            assert(response.response.tags[1] === "secondTag");
+        });
+        this.test("remove tags", async function(){
+            const response = await request("post", "/api/v1/update", {
+                ending: linkEnding,
+                tags: [],
+            });
+            assert(response.success);
+            assert(response.response.url === "http://www.maptionnaire.com");
+            assert(response.response.tags && response.response.tags.length === 0);
+            const newTime = moment(response.response.lastModifiedTime);
+            assert(newTime > lastTime);
+            lastTime = newTime;
+        });
+    });
+    
+    shorter.series("shorten one then delete", function(){
+        let linkEnding;
+        this.test("shorten", async function(){
+            const response = await request("post", "/api/v1/shorten", {
+                url: "http://www.google.com",
+            });
+            linkEnding = response.response.ending;
+            assert(response.success);
+            assert(linkEnding);
+        });
+        this.test("delete", async function(){
+            const response = await request("post", "/api/v1/delete", {
+                ending: linkEnding,
+            });
+            assert(response.success);
+        });
+        this.test("confirm deletion", async function(){
+            const response = await request("post", "/api/v1/get", {
+                ending: linkEnding,
+            });
+            assert(!response.success);
+            assert(response.error.code === 400);
+        });
+    });
+    
+    shorter.series("batch shorten", function(){
+        let links;
+        this.test("shorten", async function(){
+            const response = await request("post", "/api/v1/shorten/batch", {
+                links: [
+                    {url: "a.com"},
+                    {url: "b.com"},
+                    {url: "c.com", ending: "linkc"},
+                ],
+            });
+            assert(response.success);
+            links = response.response.links;
+            assert(links && links.length === 3);
+            assert(links[0].url === "a.com");
+            assert(links[1].url === "b.com");
+            assert(links[2].url === "c.com");
+            assert(links[2].ending === "linkc");
+        });
+        for(let i = 0; i < 3; i++){
+            this.test(`confirm creation of link #${1 + i}`, async function(){
+                const link = links[i];
+                const response = await request("post", "/api/v1/get", {
+                    ending: link.ending,
+                });
+                assert(response.success);
+                assert(response.response.url === link.url);
+            });
+        }
     });
     
     return shorter;
